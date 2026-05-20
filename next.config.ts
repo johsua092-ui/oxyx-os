137fc820b59c40c93b0b0755bd19b0f3c4d05101
johsua092-ui <johsua092@gmail.com>
2026-05-21 01:45:52 +0700

    Restrict Content Security Policy and HSTS headers according to security requirements

--- next.config.ts
+++ next.config.ts
@@ -19,23 +19,18 @@ const nextConfig: NextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com https://www.youtube.com https://open.spotify.com",
-              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "script-src 'self'",
+              "style-src 'self' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
-              "img-src 'self' data: blob: https: http:",
-              "media-src 'self' https:",
-              "frame-src https://www.youtube.com https://open.spotify.com https://*.firebaseapp.com",
+              "img-src 'self' https://firebasestorage.googleapis.com",
               "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://*.firebaseapp.com wss://*.firebaseio.com https://api.spotify.com https://accounts.spotify.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com",
               "object-src 'none'",
-              "base-uri 'self'",
-              "form-action 'self'",
-              "frame-ancestors 'none'",
               "upgrade-insecure-requests",
             ].join('; ')
           },
           {
             key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload'
+            value: 'max-age=15552000; includeSubDomains'
           },
         ],
       },
